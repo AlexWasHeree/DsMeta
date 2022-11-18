3dfb724ee26f54dfa1882f6f3035e45f998ac361
AlexWasHeree <victoralexandre2303@gmail.com>
2022-11-18 16:48:59 -0300

    query by date

--- backend/src/main/java/com/aws/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/aws/dsmeta/services/SalesService.java
@@ -1,8 +1,13 @@
 package com.aws.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.aws.dsmeta.entities.Sale;
@@ -14,7 +19,15 @@ public class SalesService {
 	@Autowired
 	private SaleRepository repo;
 	
-	public List<Sale> findSales() {
-		return repo.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repo.findSales(min, max, pageable);
 	}
+	
+	
 }
