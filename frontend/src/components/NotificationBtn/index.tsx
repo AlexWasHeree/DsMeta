520b5d0d4630a55c33e045399624d61b47c8e2e8
AlexWasHeree <victoralexandre2303@gmail.com>
2022-11-23 16:32:43 -0300

    send notification button feat

--- frontend/src/components/NotificationBtn/index.tsx
+++ frontend/src/components/NotificationBtn/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import axios from 'axios';
 import icon from '../../assets/Vector.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-const NotificationBtn = () => {
+type Props = {
+  saleId: number;
+};
+
+const NotificationBtn = ({ saleId }: Props) => {
+  function notify() {
+    axios.get(`${BASE_URL}/sales/${saleId}/notification`).then((response) => {
+      console.log('enviado');
+    });
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={notify}>
       <img src={icon} alt="Notificar" />
     </div>
   );
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -80,7 +80,7 @@ const SalesCard = () => {
                       <td>R$ {sale.amount.toFixed(2)}</td>
                       <td>
                         <div className="dsmeta-red-btn-container">
-                          <NotificationBtn />
+                          <NotificationBtn saleId={sale.id} />
                         </div>
                       </td>
                     </tr>
